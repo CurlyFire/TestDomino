55e8272555f57bf724d3086385f026cc9d1cb5cf
CurlyFire <stephane.denomme@inspq.qc.ca>
2018-08-14 14:07:26 -0400

    Add project files.

---
+++ TestDomino/Program.cs
@@ -0,0 +1,33 @@
+﻿using Domino;
+using System.Runtime.InteropServices;
+
+namespace TestDomino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotesSession session = null;
+            try
+            {
+                session = new NotesSession();
+                session.Initialize("MonPetitJulien1");
+                var database = session.GetDatabase("CancerS_A1/Serveurs/SSSS", @"TEST\PQDCS\PRINCIP.NSF", false);
+                var collection = database.AllDocuments;
+                var document = collection.GetFirstDocument();
+                while (document != null)
+                {
+                    var form = document.GetItemValue("form");
+                    foreach (NotesItem item in document.Items)
+                    {
+                    }
+                    document = collection.GetNextDocument(document);
+                }
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(session);
+            }
+        }
+    }
+}
